bd4a3d8af41c9906d722e314f7e7ed6447df8c5c
Glenn Salaman <43499190+gsalaman@users.noreply.github.com>
2026-07-09 07:18:25 -0600

    Update vault_game.py

--- vault_game.py
+++ vault_game.py
@@ -1,7 +1,25 @@
+# key data
 key_circle = neopixel.create(DigitalPin.P2, 16, NeoPixelMode.RGB)
 key_state = "wait_for_press"
 key_count = 0
 key_next_update_ms = 0
+
+# Shape data
+shape_leds = neopixel.create(DigitalPin.P0, 48, NeoPixelMode.RGB)
+# GLENN: These need to get fixed
+circle_leds = shape_leds.range(1, 5)
+square_leds = shape_leds.range(6,5)
+triangle_leds = shape_leds.range(12,3)
+heart_leds = shape_leds.range(20,3)
+
+shape_list = ["circle", "square", "triangle", "heart"]
+remaining_shapes = shape_list
+current_shape = shape_list[0]
+last_shape_time = 0
+shape_state_count = 0
+current_shape_state = "idle"
+
+# Generic defines
 rgb_green = 0x00ff00
 rgb_red = 0xff0000
 rgb_blue = 0x0000ff
@@ -62,9 +80,132 @@ def drive_key_state():
     elif (key_state == "flash"):
         key_process_flash()
 
+def shape_game_reset():
+    global remaining_shapes, shape_list, current_shape, shape_leds
+
+    # two cleanup items:  make sure the vault is closed, and make sure our strip is off
+    # may not stricly be necessary, but good defensive practice.
+    pins.digital_write_pin(DigitalPin.P8, 0)
+    shape_leds.show_color(rgb_black)
+
+    # set up our "shapes" data
+    remaining_shapes = shape_list
+    current_shape_index = randint(0,remaining_shapes.length)
+    current_shape = remaining_shapes[current_shape_index]
+
+    # show the first shape.
+    turn_shape_on(current_shape)
+    current_shape_state = "input"
+
+def get_pin_for_shape(shape):
+    if (shape == "circle"):
+        return(DigitalPin.P3)
+    elif (shape == "square"):
+        return(DigitalPin.P4)
+    elif (shape == "triangle"):
+        return(DigitalPin.P5)
+    else:  # shape == "heart"
+        return(DigitalPin.P6)
+
+# turns on a single shape
+def turn_shape_on(shape):
+    if (shape == "circle"):
+        circle_leds.show_color(rgb_yellow)
+    elif (shape == "square"):
+        square_leds.show_color(rgb_green)
+    elif (shape == "triangle"):
+        triangle_leds.show_color(rgb_blue)
+    elif (shape == "heart"):
+        heart_leds.show_color(rgb_red)
+       
+# turns off a single shape
+def turn_shape_off(shape):
+    if (shape == "circle"):
+        circle_leds.show_color(rgb_black)
+    elif (shape == "square"):
+        square_leds.show_color(rgb_black)
+    elif (shape == "triangle"):
+        triangle_leds.show_color(rgb_black)
+    elif (shape == "heart"):
+        heart_leds.show_color(rgb_black)
+
+def turn_all_shapes_on():
+    turn_shape_on("square")
+    turn_shape_on("circle")
+    turn_shape_on("triangle")
+    turn_shape_on("heart")
+
+def turn_on_all_found_shapes():
+    pass
+
+# clears all shapes
+def clear_shapes():
+    shape_leds.show_color(rgb_black)
+
+def shape_idle_state():
+    # currently we do nothing here
+    pass
+
+def shape_input_state():
+    global current_shape, current_shape_state, shape_state_count
+
+    # is the current shape button currently pressed?
+    relevant_pin = get_pin_for_shape(current_shape)
+    if (pins.digital_read_pin(relevant_pin) == 1):
+        turn_on_all_found_shapes()
+        current_shape_state = "flash"
+        shape_state_count = 0
+
+def shape_flash_state():
+    global current_shape, remaining_shapes, shape_state_count, current_shape_state, last_shape_time
+
+    time_now = input.running_time()
+    if (time_now > last_shape_time + 500):
+        # do we need to turn the symbol on or off?
+        # if we're in the bottom half of the second, we'll turn it on.
+        # top half, we'll  turn it off
+        if (time_now % 1000 < 500):
+            
+            turn_shape_on(current_shape)
+        else:
+            turn_shape_off(current_shape)
+        last_shape_time = time_now
+        
+        # if we've flashed it enough, it's time to move on 
+        shape_state_count = shape_state_count + 1
+        if (shape_state_count > 6):
+            # do we have more shapes to do?
+            remaining_shapes.remove_element(current_shape)
+            if (remaining_shapes.length > 0):
+                # pick the next shape
+                current_shape_index = randint(0,remaining_shapes.length)
+                current_shape = remaining_shapes[current_shape_index]
+                clear_shapes()
+                turn_shape_on(current_shape)
+                current_shape_state = "input"
+
+            else: 
+                # Thats all of them!  move on to the success state.
+                pass
+
+def shape_success_state():
+    pass
+
+def drive_shape_state():
+    global current_shape_state
+
+    if (current_shape_state == "idle"):
+        shape_idle_state()
+    elif (current_shape_state == "input"):
+        shape_input_state()
+    elif (current_shape_state == "flash"):
+        shape_flash_state()
+    elif (current_shape_state == "success"):
+        shape_success_state()
+
 def on_forever():
     drive_key_state()
-    
+    drive_shape_state()
 
-
+shape_game_reset()    
 basic.forever(on_forever)
